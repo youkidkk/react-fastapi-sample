ba4181d02302387edd1e90eb2428fa6f98c332dc
youkidkk <youkidkk80@gmail.com>
2022-07-05 21:38:20 +0900

    Hello を追加

---
+++ back/src/main.py
@@ -0,0 +1,26 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/hello")
+def hello():
+    return {"message": "Hello World!!!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
