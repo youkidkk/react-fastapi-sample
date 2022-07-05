207660305ba5da4bb47740e4c9f66734715a0516
youkidkk <youkidkk80@gmail.com>
2022-07-05 21:38:39 +0900

    back との通信処理で書き換え

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+
+type Response = {
+  message: string;
+};
 
 function App() {
+  const [data, setData] = React.useState<Response>();
+  const url = "http://127.0.0.1:8000/api/hello";
+
+  const GetData = () => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? (
+        <div>{data.message}</div>
+      ) : (
+        <button onClick={GetData}>データを取得</button>
+      )}
     </div>
   );
 }
